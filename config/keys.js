fc0dcf28d1fd8291a43e57602050cf7cf0078ec1
Mark Bae <markbae@Marks-MacBook-Pro.local>
2019-04-02 20:24:15 -0400

    added config files

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+const elephantURI =
+  'postgres://edqzkrel:gLFrVLsoEbS3ULiw1uonJK1irkNBdeF8@isilo.db.elephantsql.com:5432/edqzkrel';
+
+const secretOrKey = 'falafel';
+
+module.exports = { elephantURI, secretOrKey };
---
+++ config/passportConfig.js
@@ -0,0 +1,30 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const db = require('./../dB/postgresPool');
+const key = require('./../config/keys').secretOrKey;
+
+const queries = require('./../dB/postgresQueries');
+
+const opts = {};
+
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = key;
+
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      const user_id = parseInt(jwt_payload.user_id);
+      db.query(queries.selectUserById, [user_id], (err, result) => {
+        if (err) {
+          res.json({ err });
+        }
+        if (result.rows.length > 0) {
+          return done(null, result.rows[0]);
+        }
+        return done(null, false);
+      });
+      //   return done(null, jwt_payload);
+    })
+  );
+};
